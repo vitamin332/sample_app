1686e26ea639ec84b7c4ce252e29a6013b48c871
EC2 Default User <ec2-user@ip-172-31-87-216.ec2.internal>
2021-11-05 09:27:21 +0000

    [finish]2-4

--- sample_app/db/migrate/20211105085833_create_lists.rb
+++ sample_app/db/migrate/20211105085833_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-      
+
       t.string :title
       t.string :body
 
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_081409) do
+ActiveRecord::Schema.define(version: 2021_11_05_085833) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
